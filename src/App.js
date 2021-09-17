20b9df7456bb0d2c8369b8fa4535a7b976a4ffe6
Py-Ashraful <ashrafulislam180@gmail.com>
2021-09-17 22:54:09 +0600

    setup react-query

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React, { useState } from 'react';
-import Navbar from './components/Navbar';
-import Planets from './components/Planets';
-import People from './components/People';
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import Planets from "./components/Planets";
+import People from "./components/People";
+import { QueryClient, QueryClientProvider } from "react-query";
 
+const queryClient = new QueryClient();
 
 function App() {
-  let [page, setPage] = useState('planets');
+  let [page, setPage] = useState("planets");
 
   return (
-    <div className="App">
-     <h1>Star Wars Info</h1>
-      <Navbar setPage={setPage} />
-      <div className="content">
-        { page === 'planets' ? <Planets /> : <People /> }
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Star Wars Info</h1>
+        <Navbar setPage={setPage} />
+        <div className="content">
+          {page === "planets" ? <Planets /> : <People />}
+        </div>
       </div>
-    </div>
+    </QueryClientProvider>
   );
 }
 
--- src/components/Planets.js
+++ src/components/Planets.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
+import { useQuery } from "react-query";
+
+const fetchPlanets = async () => {
+  const res = await fetch(`https://swapi.dev/api/planets`);
+  return res.json();
+};
 
 const Planets = () => {
+  const { data: planets, status } = useQuery("planets", fetchPlanets);
+
+  console.info(planets);
+  console.info(status);
+
   return (
     <div>
       <h2>Planets</h2>
     </div>
   );
-}
- 
+};
+
 export default Planets;
